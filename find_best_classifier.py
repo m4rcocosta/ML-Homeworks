fb382b1b287592e8b4764c5c9a7725f13cf25a90
Marco <costa.1691388@studenti.uniroma1.it>
2019-11-08 00:25:25 +0100

    Improved code, changed classifiers

--- find_best_classifier.py
+++ find_best_classifier.py
@@ -3,18 +3,19 @@ import sys
 from os.path import join
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import LogisticRegression
-from sklearn import preprocessing
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score
 
+# Train dataset path
 train_dataset_path = "./dataset/train_dataset.jsonl"
-result_file = open("scores.txt", "a")
 
 # Read data
 def read_data():
@@ -42,7 +43,7 @@ def read_data():
     print("-"*50)
     return data
 
-#split dataset
+# Split dataset
 def split_dataset(classifier_type, instruction_type):
     target = ""
     if classifier_type == "Binary":
@@ -53,81 +54,88 @@ def split_dataset(classifier_type, instruction_type):
     encoder = preprocessing.LabelEncoder()
     y_train = encoder.fit_transform(y_train)
     y_test = encoder.fit_transform(y_test)
-    sys.stdout.write("x_train = ")
-    print(x_train.shape)
-    sys.stdout.write("y_train = ")
-    print(y_train.shape)
-    print("-"*50)
-    sys.stdout.write("x_test = ")
-    print(x_test.shape)
-    sys.stdout.write("y_test = ")
-    print(y_test.shape)
-    print("-"*50)
     return x_train, x_test, y_train, y_test
 
-#classify
+# Classify
 def classify(x_train_count, x_test_count, y_train, y_test, vectorizer, instruction_type, classifier_type):
     for classifier in classifiers:
-        print("="*50, file = result_file)
-        print("Classifier:",type(classifier).__name__,"[Vectorizer: " + vectorizer +"], [Instruction type: " + instruction_type +"], [Classifier type: " + classifier_type + "]", file = result_file)
-        print("="*50, file = result_file)
+        print("="*50, file = score_file)
+        print("Classifier:",type(classifier).__name__,"[Vectorizer: " + vectorizer +"], [Instruction type: " + instruction_type +"], [Classifier type: " + classifier_type + "]", file = score_file)
+        print("="*50, file = score_file)
         print("Fitting",type(classifier).__name__,"[Vectorizer: " + vectorizer +"], [Instruction type: " + instruction_type +"], [Classifier type: " + classifier_type + "]")
         classifier.fit(x_train_count, y_train)
         print("-"*50)
         print("Predicting",type(classifier).__name__,"[Vectorizer: " + vectorizer +"], [Instruction type: " + instruction_type +"], [Classifier type: " + classifier_type + "]")
         y_pred = classifier.predict(x_test_count)
         print("-"*50)
-        print("Result:", file = result_file)
-        print("-"*50, file = result_file)
-        print("Confusion Matrix:", file = result_file)
-        print(confusion_matrix(y_test, y_pred), file = result_file)
-        print("-"*50, file = result_file)
-        print("Classification Report:", file = result_file)
-        print(classification_report(y_test, y_pred), file = result_file)
-        print("-"*50, file = result_file)
-        print("Accuracy:", file = result_file)
-        print(accuracy_score(y_test, y_pred), file = result_file)
-        print("-"*50, file = result_file)
-        print("Precision:", file = result_file)
-        print(precision_score(y_test, y_pred, average="macro"), file = result_file)
-        print("-"*50, file = result_file)
-        print("Recall:", file = result_file)
-        print(recall_score(y_test, y_pred, average="macro"), file = result_file)
-        print("-"*50, file = result_file)
-        print("Cross Validation Score:", file = result_file)
-        print(cross_val_score(classifier, x_test_count, y_test, cv=10), file = result_file)
-        print("-"*50, file = result_file)
-        print("F1 Score:", file = result_file)
-        print(f1_score(y_test, y_pred, average="macro"), file = result_file)
-        print("-"*50, file = result_file)
+        print("Result:", file = score_file)
+        print("-"*50, file = score_file)
+        print("Confusion Matrix:", file = score_file)
+        print(confusion_matrix(y_test, y_pred), file = score_file)
+        print("-"*50, file = score_file)
+        print("Classification Report:", file = score_file)
+        print(classification_report(y_test, y_pred), file = score_file)
+        print("-"*50, file = score_file)
+        print("Accuracy:", file = score_file)
+        print(accuracy_score(y_test, y_pred), file = score_file)
+        print("-"*50, file = score_file)
+        print("Precision:", file = score_file)
+        print(precision_score(y_test, y_pred, average="macro"), file = score_file)
+        print("-"*50, file = score_file)
+        print("Recall:", file = score_file)
+        print(recall_score(y_test, y_pred, average="macro"), file = score_file)
+        print("-"*50, file = score_file)
+        print("Performing Cross Validation Score",type(classifier).__name__,"[Vectorizer: " + vectorizer +"], [Instruction type: " + instruction_type +"], [Classifier type: " + classifier_type + "]")
+        print("Cross Validation Score:", file = score_file)
+        print(cross_val_score(classifier, x_test_count, y_test, cv = 10, n_jobs = -1), file = score_file)
+        print("-"*50)
+        print("-"*50, file = score_file)
+        print("F1 Score:", file = score_file)
+        print(f1_score(y_test, y_pred, average="macro"), file = score_file)
+        print("-"*50, file = score_file)
 
-#Read data
-data = read_data()
 
-#Classifiers
+# Classifiers
 classifier_types = ["Binary", "Multiclass"]
-classifiers = [LogisticRegression(random_state=0, solver='lbfgs', multi_class='auto', max_iter=5000), MultinomialNB(), SVC(kernel="rbf", gamma="scale")]
+classifiers = [LogisticRegression(random_state=0, solver="lbfgs", multi_class="auto", max_iter=5000, n_jobs=-1), MultinomialNB(), DecisionTreeClassifier(random_state=0), SVC(kernel="rbf", gamma="scale"), KNeighborsClassifier(n_neighbors=3, n_jobs=-1), RandomForestClassifier(n_estimators=100, n_jobs=-1)]
 
-#Vectorizers
-count_vect = CountVectorizer(input='content', encoding='utf-8', tokenizer=lambda x: x.split(','))
-tfidt_vect = TfidfVectorizer(analyzer='word', token_pattern=r'([^,]+)')
-tfidt_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'([^,]+)',  ngram_range=(3,3))
-vectorizers = [count_vect, tfidt_vect, tfidt_vect_ngram]
+# Vectorizers
+vectorizers = [CountVectorizer(input="content", encoding="utf-8", tokenizer=lambda x: x.split(",")), TfidfVectorizer(analyzer="word", token_pattern=r"([^,]+)"), CountVectorizer(input="content", encoding="utf-8", tokenizer=lambda x: x.split(","),  ngram_range=(3,3)), TfidfVectorizer(analyzer="word", token_pattern=r"([^,]+)",  ngram_range=(3,3)), CountVectorizer(input="content", encoding="utf-8", tokenizer=lambda x: x.split(","),  ngram_range=(4,4)), TfidfVectorizer(analyzer="word", token_pattern=r"([^,]+)",  ngram_range=(4,4))]
 
+# Instruction types
 instruction_types = ["mnemonics", "instructions"]
 
+# Main
+if len(sys.argv) < 3:
+    print("Bad usage! You must specify the classifier type (Binary/Multiclass) and the instruction type (mnemonics/instructions).")
+    exit(1)
+classifier_type = sys.argv[1]
+instruction_type = sys.argv[2]
+if classifier_type not in classifier_types:
+    print("Bad usage! Classifier type must be 'Binary' or 'Multiclass'.")
+    exit(1)
+if instruction_type not in instruction_types:
+    print("Bad usage! Instruction type must be 'mnemonics' or 'instructions'.")
+    exit(1)
 
-for classifier_type in classifier_types:
-    for instruction_type in instruction_types:
-        #Split dataset
-        x_train, x_test, y_train, y_test = split_dataset(classifier_type, instruction_type)
-        for vectorizer in vectorizers:
-            vectorizer.fit(data[instruction_type])
-            x_train_count = vectorizer.transform(x_train)
-            x_test_count = vectorizer.transform(x_test)
-            name = type(vectorizer).__name__
-            if vectorizer == tfidt_vect_ngram:
-                name += " Ngmam Range"
-            classify(x_train_count, x_test_count, y_train, y_test, name, instruction_type, classifier_type)
+# Read data
+data = read_data()
+print(data)
+print()
 
-result_file.close()
+score_file = open("./scores/" + classifier_type + "_" + instruction_type + "_scores.txt", "w")
+
+# Split dataset
+x_train, x_test, y_train, y_test = split_dataset(classifier_type, instruction_type)
+for vectorizer in vectorizers:
+    vectorizer.fit(data[instruction_type])
+    x_train_count = vectorizer.transform(x_train)
+    x_test_count = vectorizer.transform(x_test)
+    name = type(vectorizer).__name__
+    if vectorizer == vectorizers[2] or vectorizer == vectorizers[3]:
+        name += " Ngram Range 3"
+    elif vectorizer == vectorizers[4] or vectorizer == vectorizers[5]:
+        name += " Ngram Range 4"
+    classify(x_train_count, x_test_count, y_train, y_test, name, instruction_type, classifier_type)
+
+score_file.close()
--- predict.py
+++ predict.py
@@ -3,12 +3,15 @@ import sys
 from os.path import join
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
-from sklearn.model_selection import cross_val_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 train_dataset_path = "./dataset/train_dataset.jsonl"
 test_dataset_path = "./dataset/test_dataset_blind.jsonl"
@@ -62,7 +65,7 @@ def read_data():
     print("-"*50)
     return train_dataset, test_dataset
 
-#split dataset
+# Split dataset
 def split_dataset(classifier_type, encoder):
     target = ""
     if classifier_type == "Binary":
@@ -71,53 +74,116 @@ def split_dataset(classifier_type, encoder):
         target = "compiler"
     x_train, x_test, y_train, y_test = train_test_split(train_dataset["instructions"], train_dataset[target], test_size = 0.20)
     y_train = encoder.fit_transform(y_train)
-    sys.stdout.write("x_train = ")
-    print(x_train.shape)
-    sys.stdout.write("y_train = ")
-    print(y_train.shape)
-    print("-"*50)
     return x_train, y_train
 
-#classify
+# Classify
 def classify(x_train_count, y_train, x_to_predict, classifier, classifier_type, encoder):
     print("="*50)
     print("[Classifier type: " + classifier_type + "]")
     print("="*50)
     print("Fitting...")
-    svc.fit(x_train_count, y_train)
+    classifier.fit(x_train_count, y_train)
     print("-"*50)
     print("Predicting...")
-    y_pred = svc.predict(x_to_predict)
+    y_pred = classifier.predict(x_to_predict)
     y_pred = encoder.inverse_transform(y_pred)
     print("-"*50)
     result[classifier_type] = y_pred
 
-#Read data
-train_dataset, test_dataset = read_data()
-
-#Classifiers
+# Classifiers
 classifier_types = ["Binary", "Multiclass"]
+classifiers = [LogisticRegression(random_state=0, solver="lbfgs", multi_class="auto", max_iter=5000, n_jobs=-1), MultinomialNB(), DecisionTreeClassifier(random_state=0), SVC(kernel="rbf", gamma="scale"), KNeighborsClassifier(n_neighbors=3, n_jobs=-1), RandomForestClassifier(n_estimators=100, n_jobs=-1)]
 
-tfidt_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'([^,]+)',  ngram_range=(3,3))
-tfidt_vect_ngram.fit(train_dataset["instructions"])
+# Vectorizers
+vectorizers = [CountVectorizer(input="content", encoding="utf-8", tokenizer=lambda x: x.split(",")), TfidfVectorizer(analyzer="word", token_pattern=r"([^,]+)"), CountVectorizer(input="content", encoding="utf-8", tokenizer=lambda x: x.split(","),  ngram_range=(3,3)), TfidfVectorizer(analyzer="word", token_pattern=r"([^,]+)",  ngram_range=(3,3)), CountVectorizer(input="content", encoding="utf-8", tokenizer=lambda x: x.split(","),  ngram_range=(4,4)), TfidfVectorizer(analyzer="word", token_pattern=r"([^,]+)",  ngram_range=(4,4))]
 
-svc = SVC(kernel="rbf", gamma="scale")
+# Main
+if len(sys.argv) < 5:
+    print("Bad usage! You must specify the classifiers and the vectorizers for binary and multi-class classification.")
+    exit(1)
+binary_classifier_index = sys.argv[1]
+multiclass_classifier_index = sys.argv[3]
+if not binary_classifier_index.isdigit() or not multiclass_classifier_index.isdigit():
+    print("Bad usage! Classifier must be an integer:")
+    print("0 - Logistic Regression")
+    print("1 - Multinomial NB")
+    print("2 - Decision Tree")
+    print("3 - SVC")
+    print("4 - K-Neighbors")
+    print("5 - Random Forest")
+    exit(1)
+binary_classifier_index = int(binary_classifier_index)
+multiclass_classifier_index = int(multiclass_classifier_index)
+if binary_classifier_index < 0 or binary_classifier_index > 5 or multiclass_classifier_index < 0 or multiclass_classifier_index > 5:
+    print("Bad usage! Classifier must be an integer:")
+    print("0 - Logistic Regression")
+    print("1 - Multinomial NB")
+    print("2 - Decision Tree")
+    print("3 - SVC")
+    print("4 - K-Neighbors")
+    print("5 - Random Forest")
+    exit(1)
+binary_vectorizer_index = sys.argv[2]
+multiclass_vectorizer_index = sys.argv[4]
+if not binary_vectorizer_index.isdigit() or not multiclass_vectorizer_index.isdigit():
+    print("Bad usage! Vectorizer must be an integer:")
+    print("0 - Count Vectorizer")
+    print("1 - Tfidf Vectorizer")
+    print("2 - Count Vectorizer with ngram_range=(3,3)")
+    print("3 - Tfidf Vectorizer with ngram_range=(3,3)")
+    print("4 - Count Vectorizer with ngram_range=(4,4)")
+    print("5 - Tfidf Vectorizer with ngram_range=(4,4)")
+    exit(1)
+binary_vectorizer_index = int(binary_vectorizer_index)
+multiclass_vectorizer_index = int(multiclass_vectorizer_index)
+if binary_vectorizer_index < 0 or binary_vectorizer_index > 5 or multiclass_vectorizer_index < 0 or multiclass_vectorizer_index > 5:
+    print("Bad usage! Vectorizer must be an integer:")
+    print("0 - Count Vectorizer")
+    print("1 - Tfidf Vectorizer")
+    print("2 - Count Vectorizer with ngram_range=(3,3)")
+    print("3 - Tfidf Vectorizer with ngram_range=(3,3)")
+    print("4 - Count Vectorizer with ngram_range=(4,4)")
+    print("5 - Tfidf Vectorizer with ngram_range=(4,4)")
+    exit(1)
+binary_classifier = classifiers[binary_classifier_index]
+multiclass_classifier = classifiers[multiclass_classifier_index]
+binary_vectorizer = vectorizers[binary_vectorizer_index]
+multiclass_vectorizer = vectorizers[multiclass_vectorizer_index]
 
-for classifier_type in classifier_types:
-    #Split dataset
-    encoder = preprocessing.LabelEncoder()
-    x_train, y_train = split_dataset(classifier_type, encoder)
-    x_train_count = tfidt_vect_ngram.transform(x_train)
-    x_to_predict = tfidt_vect_ngram.transform(test_dataset["instructions"])
-    classify(x_train_count, y_train, x_to_predict, svc, classifier_type, encoder)
+# Read data
+train_dataset, test_dataset = read_data()
+print(train_dataset)
+print(test_dataset)
+print()
 
-print("Writing scv file...")
+# Encoder
+encoder = preprocessing.LabelEncoder()
+
+# Binary classification
+print("Performing binary classification...")
+x_train, y_train = split_dataset("Binary", encoder)
+binary_vectorizer.fit(train_dataset["instructions"])
+x_train_count = binary_vectorizer.transform(x_train)
+x_to_predict = binary_vectorizer.transform(test_dataset["instructions"])
+classify(x_train_count, y_train, x_to_predict, binary_classifier, "Binary", encoder)
+
+# Multiclass classification
+print("Performing multi-class classification...")
+x_train, y_train = split_dataset("Multiclass", encoder)
+multiclass_vectorizer.fit(train_dataset["instructions"])
+x_train_count = multiclass_vectorizer.transform(x_train)
+x_to_predict = multiclass_vectorizer.transform(test_dataset["instructions"])
+classify(x_train_count, y_train, x_to_predict, multiclass_classifier, "Multiclass", encoder)
+
+# Create csv file
+print("Writing csv file...")
 low_count = 0
 high_count = 0
 gcc_count = 0
 icc_count = 0
 clang_count = 0
-predictions_file = open("predictions.csv", "w")
+predictions_file = open("./predictions/predictions.csv", "w")
+count_file = open("./predictions/count.txt", "w")
 for i in range(len(result["Binary"])):
     print(result["Multiclass"][i] + "," + result["Binary"][i], file = predictions_file)
     if result["Binary"][i] == "L":
@@ -130,8 +196,9 @@ for i in range(len(result["Binary"])):
         icc_count += 1
     else:
         clang_count += 1
+print("Binary classification result:", file = count_file)
+print("L: " + str(low_count) + ", H: " + str(high_count), file = count_file)
+print("Multiclass classification result:", file = count_file)
+print("gcc: " + str(gcc_count) + ", icc: " + str(icc_count) + ", clang: " + str(clang_count), file = count_file)
 predictions_file.close()
-print("Binary classification result:")
-print("L: " + str(low_count) + ", H: " + str(high_count))
-print("Multiclass classification result:")
-print("gcc: " + str(gcc_count) + ", icc: " + str(icc_count) + ", clang: " + str(clang_count))
+count_file.close()
